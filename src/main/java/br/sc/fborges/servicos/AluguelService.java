df3e18a8ad3522e1cf83866f8307a64bdd8f3860
Felipe Rubel Borges <felpe.rubel@hotmail.com>
2018-11-17 21:32:20 -0200

    atualizacoes BDD

--- src/main/java/br/sc/fborges/servicos/AluguelService.java
+++ src/main/java/br/sc/fborges/servicos/AluguelService.java
@@ -2,28 +2,37 @@ package br.sc.fborges.servicos;
 
 import br.sc.fborges.entidades.Filme;
 import br.sc.fborges.entidades.NotaAluguel;
+import br.sc.fborges.entidades.TipoAluguel;
 import br.sc.fborges.utils.DateUtils;
 
 public class AluguelService {
 
-	public NotaAluguel alugar(Filme filme, String tipo) {
+	public NotaAluguel alugar(Filme filme, TipoAluguel tipo) {
 		if (filme.getEstoque() == 0) {
 			throw new RuntimeException("Filme sem Estoque");
 		}
 		NotaAluguel nota = new NotaAluguel();
-		if ("extendido".equals(tipo)) {
-			nota.setPreço(filme.getAluguel() * 2);
-			nota.setDataEntrega(DateUtils.obterDataDiferencaDias(3));
-			nota.setPontuacao(2);
-		} else if ("semanal".equals(tipo)) {
-			nota.setPreço(filme.getAluguel() * 3);
-			nota.setDataEntrega(DateUtils.obterDataDiferencaDias(7));
-			nota.setPontuacao(3);
-		} else {
+
+		switch (tipo) {
+		case COMUM:
 			nota.setPreço(filme.getAluguel());
 			nota.setDataEntrega(DateUtils.obterDataDiferencaDias(1));
 			nota.setPontuacao(1);
+			break;
+		case EXTENDIDO:
+			nota.setPreço(filme.getAluguel() * 2);
+			nota.setDataEntrega(DateUtils.obterDataDiferencaDias(3));
+			nota.setPontuacao(2);
+			break;
+		case SEMANAL:
+			nota.setPreço(filme.getAluguel() * 3);
+			nota.setDataEntrega(DateUtils.obterDataDiferencaDias(7));
+			nota.setPontuacao(3);
+			break;
+		default:
+
 		}
+
 		filme.setEstoque(filme.getEstoque() - 1);
 		return nota;
 
--- src/test/java/br/sc/fborges/steps/AlugarFilmeSteps.java
+++ src/test/java/br/sc/fborges/steps/AlugarFilmeSteps.java
@@ -4,13 +4,16 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Map;
 
 import org.junit.Assert;
 
 import br.sc.fborges.entidades.Filme;
 import br.sc.fborges.entidades.NotaAluguel;
+import br.sc.fborges.entidades.TipoAluguel;
 import br.sc.fborges.servicos.AluguelService;
 import br.sc.fborges.utils.DateUtils;
+import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
@@ -22,7 +25,7 @@ public class AlugarFilmeSteps {
 	private AluguelService aluguel = new AluguelService();
 	private NotaAluguel nota;
 	private String erro;
-	private String tipoAluguel;
+	private TipoAluguel tipoAluguel;
 
 	@Dado("^um filme com estoque de (\\d+) unidades$")
 	public void umFilmeComEstoqueDeUnidades(int arg1) throws Throwable {
@@ -35,6 +38,17 @@ public class AlugarFilmeSteps {
 	public void queOPreçoDoAluguelSejaR$(int arg1) throws Throwable {
 		filme.setAluguel(arg1);
 
+	}
+
+	@Dado("^um filme$")
+	public void umFilme(DataTable table) throws Throwable {
+		Map<String, String> map = table.asMap(String.class, String.class);
+		filme = new Filme();
+		filme.setEstoque(Integer.parseInt(map.get("estoque")));
+		filme.setAluguel(Integer.parseInt(map.get("preço")));
+		String tipo = map.get("tipo");
+		tipoAluguel = tipo.equals("semanal") ? TipoAluguel.SEMANAL
+				: tipo.equals("extendido") ? TipoAluguel.EXTENDIDO : TipoAluguel.COMUM;
 	}
 
 	@Quando("^alugar$")
@@ -67,7 +81,8 @@ public class AlugarFilmeSteps {
 
 	@Dado("^que o tipo do aluguel seja (.*)$")
 	public void queOTipoDoAluguelSejaExtendido(String tipo) throws Throwable {
-		tipoAluguel = tipo;
+		tipoAluguel = tipo.equals("semanal") ? TipoAluguel.SEMANAL
+				: tipo.equals("extendido") ? TipoAluguel.EXTENDIDO : TipoAluguel.COMUM;
 	}
 
 	@Então("^a data de entrega será em (\\d+) dias?$")
@@ -82,8 +97,7 @@ public class AlugarFilmeSteps {
 
 	@Então("^a pontuação será de (\\d+) pontos$")
 	public void aPontuaçãoSeráDePontos(int arg1) throws Throwable {
-	    Assert.assertEquals(arg1, nota.getPontuacao());
+		Assert.assertEquals(arg1, nota.getPontuacao());
 	}
 
-	
 }
